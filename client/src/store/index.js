6f30b13f79256848c70f222ee586bc7cec003fa8
Chikkii <chiragrupareliya1005@gmail.com>
2023-05-08 14:58:05 +0530

    fixed deployment2

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
   color: '#EFBD48',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png',
+  logoDecal: './pxo.png',
+  fullDecal: './pxo.png',
 });
 
 export default state;
